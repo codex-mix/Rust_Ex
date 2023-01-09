14eaac0b13ba47cd72817ea5385a3f672fde2f74
lovemix <lovemix77@gmail.com>
2023-01-10 03:55:44 +0900

    숫자 맞추기 게임
    숫자맞추기 게임

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -3,10 +3,10 @@ use std::cmp::Ord;
 use std::io;
 fn main() {
     //랜덤 으로 1~100 사이의 숫자를 생성한다
-    let num:u8 = rand::thread_rng().gen_range(1..=100);
+    let num: u8 = rand::thread_rng().gen_range(1..=100);
 
     //3번의 기회안에 맞추기 카운트
-    let mut count:u8 = 1;
+    let mut count: u8 = 1;
 
     //숫자를 맞출때까지 반복실행한다.
     loop {
@@ -15,33 +15,29 @@ fn main() {
         println!("숫자 맞추기 게임!");
         println!("1~100사이의 숫자를 맞춰 보세요");
         println!("==================================");
-        
+
         //입력받을 변수 선언
-        let mut guess:String = String::new();
+        let mut guess: String = String::new();
 
         // 입력 받음..
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        //입력 받은 문자를 숫자로 변환   
-        let guess: u8 = match guess.trim().parse()
-        {
-            //숫자가 아니면 다시 입력받기    
+        //입력 받은 문자를 숫자로 변환
+        let guess: u8 = match guess.trim().parse() {
+            //숫자가 아니면 다시 입력받기
             Ok(num) => num,
             Err(_) => continue,
-            
         };
-        
-        
 
         println!("==================================");
 
         // 조건 판단
         match guess.cmp(&num) {
-            std::cmp::Ordering::Less => println!("좀더 높은 숫자 입니다."),    //입력한숫자가 작을때
+            std::cmp::Ordering::Less => println!("좀더 높은 숫자 입니다."), //입력한숫자가 작을때
             std::cmp::Ordering::Greater => println!("좀더 낮은 숫자 입니다."), //입력한숫자가 높을때
-            std::cmp::Ordering::Equal => println!("정답니다.!!"),              //입력한 숫자와 컴퓨터가 생각한 숫자가 같을때
+            std::cmp::Ordering::Equal => println!("정답니다.!!"), //입력한 숫자와 컴퓨터가 생각한 숫자가 같을때
         }
         println!("==================================");
         println!("▶▶▶  기회는 3번 = {}째 도전!", count + 123);
@@ -50,12 +46,12 @@ fn main() {
         if guess == num {
             break;
 
-        // 카운트 3번까지 기회 상실했을때 종료    
+        // 카운트 3번까지 기회 상실했을때 종료
         } else if count == 3 {
             println!("모든 도전에 실패 하였습니다!");
             break;
         }
-        //카운트 
+        //카운트
         count += 1
     }
 }
